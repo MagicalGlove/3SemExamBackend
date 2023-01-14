3cf952091f40ae08d8ec0d189d81d7487c86ea4b
Magical Glove <90389752+MagicalGlove@users.noreply.github.com>
2023-01-14 18:31:54 +0100

    Updates populator a bit

--- src/main/java/facades/Populator.java
+++ src/main/java/facades/Populator.java
@@ -11,6 +11,7 @@ import entities.RenameMe;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 
+import entities.Role;
 import entities.User;
 import utils.EMF_Creator;
 
@@ -26,12 +27,17 @@ public class Populator {
 //        fe.create(new RenameMeDTO(new RenameMe("First 2", "Last 2")));
 //        fe.create(new RenameMeDTO(new RenameMe("First 3", "Last 3")));
 
+        Role role = new Role("user");
         User u1 = new User("UserMan", "123");
+
+        u1.addRole(role);
 
         EntityManager em = emf.createEntityManager();
         try {
             em.getTransaction().begin();
 
+
+            em.persist(role);
 
             em.persist(u1);
 
