a7430758f7a17c2e8212109571cff774a300e3f4
Magical Glove <90389752+MagicalGlove@users.noreply.github.com>
2023-01-18 09:14:38 +0100

    adds admin role

--- src/main/java/facades/Populator.java
+++ src/main/java/facades/Populator.java
@@ -15,6 +15,11 @@ import entities.Role;
 import entities.User;
 import utils.EMF_Creator;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  *
  * @author tha
@@ -23,22 +28,30 @@ public class Populator {
     public static void populate(){
         EntityManagerFactory emf = EMF_Creator.createEntityManagerFactory();
         FacadeExample fe = FacadeExample.getFacadeExample(emf);
+
 //        fe.create(new RenameMeDTO(new RenameMe("First 1", "Last 1")));
 //        fe.create(new RenameMeDTO(new RenameMe("First 2", "Last 2")));
 //        fe.create(new RenameMeDTO(new RenameMe("First 3", "Last 3")));
 
-        Role role = new Role("user");
-        User u1 = new User("UserTime", "123");
 
-        u1.addRole(role);
+
+        Role userRole = new Role("user");
+        Role adminRole = new Role("admin");
+        User u1 = new User("UserTime", "123");
+        User a1 = new User("AdminTime", "123");
+
+        u1.addRole(userRole);
+        a1.addRole(adminRole);
 
         EntityManager em = emf.createEntityManager();
         try {
             em.getTransaction().begin();
 
-            em.persist(role);
+            em.persist(userRole);
+            em.persist(adminRole);
 
             em.persist(u1);
+            em.persist(a1);
 
             em.getTransaction().commit();
         } finally {
